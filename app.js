733981cb40d75ddc9d15c81126b2b3ed471e6986
zaibali <zaibali51@live.com>
2024-03-03 09:04:26 +0500

    Hello Js Planet

---
+++ app.js
@@ -0,0 +1,7 @@
+document.write("<h1>Hello <span style='color:green;'>JavaScript</span> Planet</h1>");
+
+document.write('<p style="font-size:1.5rem;">I am the <span style="font-weight:bold;">master</span> of my own fate.</p>');
+
+document.write('<p style="font-size:1.5rem;">I am the <span style="font-weight:bold;">champion</span> of my own soul.</p>');
+
+document.write('<p style="font-size:1.5rem;">I am the <span style="font-weight:bold;">superhero</span> of my own life.</p>');
